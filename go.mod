842a03a59718770be3f723246dab71ee61f81665
Maksim Fisenko <fisenkomaksim.id@gmail.com>
2025-07-14 21:33:57 +0300

    feat(generator): add more variables

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/maksimfisenko/moxer
 go 1.24.4
 
 require (
+	github.com/brianvoe/gofakeit/v7 v7.3.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
 	github.com/labstack/echo/v4 v4.13.4
--- internal/services/generator/generator.go
+++ internal/services/generator/generator.go
@@ -3,16 +3,64 @@ package generator
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
-	"github.com/google/uuid"
+	"github.com/brianvoe/gofakeit/v7"
 )
 
 var varPattern = regexp.MustCompile(`{{\s*([a-zA-Z0-9_]+)\s*}}`)
 
 var generators = map[string]func() any{
-	"uuid": func() any { return uuid.New().String() },
-	"name": func() any { return "random_name" },
+	// Misc
+	"uuid": func() any { return gofakeit.UUID() },
+	"bool": func() any { return strconv.FormatBool(gofakeit.Bool()) },
+	// Person
+	"name":        func() any { return gofakeit.Name() },
+	"first_name":  func() any { return gofakeit.FirstName() },
+	"middle_name": func() any { return gofakeit.MiddleName() },
+	"last_name":   func() any { return gofakeit.LastName() },
+	"phone":       func() any { return gofakeit.Phone() },
+	"email":       func() any { return gofakeit.Email() },
+	"username":    func() any { return gofakeit.Username() },
+	"password":    func() any { return gofakeit.Password(true, true, true, true, false, 8) },
+	"gender":      func() any { return gofakeit.Gender() },
+	// Address
+	"country":    func() any { return gofakeit.Country() },
+	"city":       func() any { return gofakeit.City() },
+	"street":     func() any { return gofakeit.Street() },
+	"zip":        func() any { return gofakeit.Zip() },
+	"latitude":   func() any { return fmt.Sprintf("%f", gofakeit.Latitude()) },
+	"longtitude": func() any { return fmt.Sprintf("%f", gofakeit.Longitude()) },
+	// Words
+	"word":        func() any { return gofakeit.Word() },
+	"noun":        func() any { return gofakeit.Noun() },
+	"verb":        func() any { return gofakeit.Verb() },
+	"adverb":      func() any { return gofakeit.Adverb() },
+	"preposition": func() any { return gofakeit.Preposition() },
+	"adjective":   func() any { return gofakeit.Adjective() },
+	"pronoun":     func() any { return gofakeit.Pronoun() },
+	"phrase":      func() any { return gofakeit.Phrase() },
+	"question":    func() any { return gofakeit.Question() },
+	// Colors
+	"color": func() any { return gofakeit.Color() },
+	"hex":   func() any { return gofakeit.HexColor() },
+	// Internet
+	"url":           func() any { return gofakeit.URL() },
+	"domain_name":   func() any { return gofakeit.DomainName() },
+	"domain_suffix": func() any { return gofakeit.DomainSuffix() },
+	"ipv4":          func() any { return gofakeit.IPv4Address() },
+	"ipv6":          func() any { return gofakeit.IPv6Address() },
+	// Date/time
+	"date":        func() any { return gofakeit.Date().String() },
+	"past_date":   func() any { return gofakeit.PastDate().String() },
+	"future_date": func() any { return gofakeit.FutureDate().String() },
+	"year":        func() any { return strconv.Itoa(gofakeit.Year()) },
+	"month":       func() any { return strconv.Itoa(gofakeit.Month()) },
+	"weekday":     func() any { return gofakeit.WeekDay() },
+	"hour":        func() any { return strconv.Itoa(gofakeit.Hour()) },
+	"minute":      func() any { return strconv.Itoa(gofakeit.Minute()) },
+	"second":      func() any { return strconv.Itoa(gofakeit.Second()) },
 }
 
 func GenerateData(content map[string]any, count int) []map[string]any {
